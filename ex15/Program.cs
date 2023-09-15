9688e02438683af7471b2f9dcce27a3e7f2444d7
Sofiya <sk074866336@gmail.com>
2023-09-15 18:23:20 +0300

    added ex 15

---
+++ ex15/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Write a number: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a>7 || a<1)
+{
+    Console.Write("It is not the day of a week!");
+}
+
+else
+{
+    if (a>5)
+    {
+        Console.Write("yes");
+    }
+    
+    else
+    {
+        Console.Write("no");
+    }
+}
